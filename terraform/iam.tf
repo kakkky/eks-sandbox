b6468ffbcfd1d7f21bfce5aa2a5f592e23d4f953
YutaKakiki <uta31501224@gmail.com>
2025-11-17 01:57:20 +0900

    🧱 Add IAM role for eks node group

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -6,7 +6,7 @@ resource "aws_iam_role" "eks_cluster_role" {
 
 data "aws_iam_policy_document" "eks_cluster_assume_role_policy" {
   statement {
-    sid    = "AllowEKSServiceAssumeRole"
+    sid    = "AllowEKSClusterAssumeRole"
     effect = "Allow"
     principals {
       type        = "Service"
@@ -23,4 +23,37 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   role       = aws_iam_role.eks_cluster_role.name
 }
 
+# IAM role for EKS Node Group
+resource "aws_iam_role" "eks_node_group_role" {
+  name               = "eks-node-group-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_node_group_assume_role_policy.json
+}
 
+data "aws_iam_policy_document" "eks_node_group_assume_role_policy" {
+  statement {
+    sid    = "AllowEKSNodeGroupAssumeRole"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
