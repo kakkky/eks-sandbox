cedbb2488914ed8d8764df988549a6e6e171b5e1
YutaKakiki <uta31501224@gmail.com>
2025-11-22 13:41:09 +0900

    🚚 Move iam decl to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,4 +82,80 @@ module "eks" {
   }
 }
 
+# IAM role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role_policy.json
+}
 
+data "aws_iam_policy_document" "eks_cluster_assume_role_policy" {
+  statement {
+    sid    = "AllowEKSClusterAssumeRole"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# IAM role for EKS Node Group
+resource "aws_iam_role" "eks_node_group_role" {
+  name               = "eks-node-group-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_node_group_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_node_group_assume_role_policy" {
+  statement {
+    sid    = "AllowEKSNodeGroupAssumeRole"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+# IAM role for developer access
+resource "aws_iam_role" "developer_role" {
+  name               = "developer-role"
+  assume_role_policy = data.aws_iam_policy_document.developer_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "developer_assume_role_policy" {
+  statement {
+    sid    = "AllowAssumeRoleForDeveloper"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::846869429016:user/yuta"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
