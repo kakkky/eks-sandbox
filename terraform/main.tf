2be3f3be2efc25458d0484f9a25cccd96fb8601b
YutaKakiki <uta31501224@gmail.com>
2025-11-29 13:46:49 +0900

    🧱 Increased az, subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,9 @@ module "vpc" {
   name = "eks-sandbox-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-1a"]
-  public_subnets  = ["10.0.0.0/24"]
-  private_subnets = ["10.0.1.0/24"]
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
+  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
 
   tags = {
     Name        = "eks-sandbox-vpc"
