4109e68cbd564e457423478f0806c8c0288fcb04
YutaKakiki <uta31501224@gmail.com>
2025-11-30 17:17:59 +0900

    🧱 Add NAT gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,9 @@ module "vpc" {
   public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
   private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
   tags = {
     Name        = "eks-sandbox-vpc"
     Terraform   = "true"
